67503981dcb78a62a856fa3b2ba1870558f24714
Zhilevan <zhilevan@gmail.com>
2017-08-06 00:11:57 +0430

    fix: update homepage content

---
+++ application/views/templates/container.php
@@ -0,0 +1,15 @@
+
+
+<div class="container">
+
+  <div class="starter-template">
+    <h1>Welcome To Test MotorBike</h1>
+    <strong>You can proceed with following items</strong>
+    <ul class="text-left ">
+        <li> <h3 class=""><a href="<?=site_url("products")?>"> List of Bikes </a></h3><p>show  list of Bikes </p></li>
+        <li > <h3  ><a href="<?=site_url("register")?>"> Sign Up </a></h3><p>in order to add motor bikes you must signup</p></li>
+        <li> <h3 class=""><a href="<?=site_url("login")?>"> Sign In </a></h3><p>if you signned up before you can login </p></li>
+    </ul>
+  </div>
+
+</div><!-- /.container -->
